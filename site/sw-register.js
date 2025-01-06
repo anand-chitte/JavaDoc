0d2024b2ce4343ba57a9d0f95d271398f6a23fd4
Anand Chitte <anandchitte973@gmail.com>
2025-01-06 15:48:14 +0530

    remove service worker

--- site/sw-register.js
+++
@@ -1,8 +0,0 @@
-navigator.serviceWorker
-  .register('/service-worker.js') // Ensure this matches the path
-  .then((registration) => {
-    console.log('Service Worker registered:', registration.scope);
-  })
-  .catch((error) => {
-    console.error('Service Worker registration failed:', error);
-  });
